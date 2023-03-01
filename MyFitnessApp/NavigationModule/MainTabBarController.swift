0790e8040f84b1f04d6f2e09d0ed3c6ddfe41b24
zcredi <zcredi@gmail.com>
2023-03-01 16:33:35 +0300

    add UITabBarController

--- MyFitnessApp/NavigationModule/MainTabBarController.swift
+++ MyFitnessApp/NavigationModule/MainTabBarController.swift
@@ -15,12 +15,29 @@ class MainTabBarController: UITabBarController {
         setupTabBar()
         setupItems()
     }
-
+    
     private func setupTabBar() {
-        
+        tabBar.backgroundColor = .specialTabBar
+        tabBar.tintColor = .specialDarkGreen
+        tabBar.unselectedItemTintColor = .specialGray
+        tabBar.layer.borderWidth = 1
+        tabBar.layer.borderColor = UIColor.specialLightBrown.cgColor
     }
     
     private func setupItems() {
+        let mainVC = MainViewController()
+        let statisticVC = StatisticViewController()
         
+        setViewControllers([mainVC, statisticVC], animated: true)
+        
+        guard let items = tabBar.items else { return }
+        
+        items[0].title = "Main"
+        items[1].title = "Statistic"
+        
+        items[0].image = UIImage(named: "mainTabBar")
+        items[1].image = UIImage(named: "statisticTabBar")
+        
+        UITabBarItem.appearance().setTitleTextAttributes([.font : UIFont(name: "Roboto-Bold", size: 16) as Any], for: .normal)
     }
 }
